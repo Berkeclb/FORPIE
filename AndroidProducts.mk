b8dff3326742926fd9cfe73a6b1e6fcf1f53511c
henricaodopao <allricknation@gmail.com>
2021-02-18 15:50:09 +0000

    X00QD: Potato bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/dot_X00QD.mk
+    $(LOCAL_DIR)/potato_X00QD.mk
 
 COMMON_LUNCH_CHOICES := \
-    dot_X00QD-userdebug \
-    dot_X00QD-eng
+    potato_X00QD-userdebug \
+    potato_X00QD-eng
